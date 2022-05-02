4344100a869a07c48fbf784dc1d9ace377afc2e4
rhiskey <first3395@gmail.com>
2022-05-02 14:40:43 +0300

    Initial Commit

---
+++ TransparentSliderValidator/TransparentSliderValidatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransparentSliderValidatorApp.swift
+//  TransparentSliderValidator
+//
+//  Created by Владимир Киселев on 02.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TransparentSliderValidatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
