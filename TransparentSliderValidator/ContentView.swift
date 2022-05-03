f8b75487e4b5ce33536d676d6784aeb3163e745f
rhiskey <first3395@gmail.com>
2022-05-03 16:07:53 +0300

    Task done

--- TransparentSliderValidator/ContentView.swift
+++ TransparentSliderValidator/ContentView.swift
@@ -8,26 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var value: Float = 50
+    @State private var targetValue: Int = 75
+    @State private var currentValue: Double = 0.5
+    @State private var showErrorAlert = false
+    @State private var showCorrectAlert = false
+    
+    private let upperBorder = 100.0
+    private let lowerBorder = 0.0
     
     var body: some View {
-        TransparencySlider(value: $value)
-            .padding()
-            .onChange(of: value) { newValue in
-                withAnimation {
-                    check(newValue)
-                }
+        VStack(spacing: 40) {
+            Text("Подвиньте слайдер как можно ближе к: \(targetValue)")
+            
+            SliderView(
+                currentValue: $currentValue,
+                targetValue: $targetValue,
+                lowerBorder: lowerBorder,
+                upperBorder: upperBorder
+            )
+            
+            Button("Проверь меня!") {
+                check(currentValue)
             }
+            .alert("Wrong! Please slide \(lround(currentValue * upperBorder)) to \(targetValue)", isPresented: $showErrorAlert, actions: { })
+            .alert("Correct!", isPresented: $showCorrectAlert, actions: { })
+            
+            Button("Начать заново") {
+                targetValue = Int.random(in: Int(lowerBorder)...Int(upperBorder))
+            }
+        }
     }
+    
 }
 
 extension ContentView {
-    private func check(_ value: Float) {
+    private func check(_ value: Double) {
+        if (targetValue-5...targetValue+5).contains(lround(value * upperBorder)) {
+            showCorrectAlert.toggle()
+        } else {
+            showErrorAlert.toggle()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SliderView: View {
+    @Binding var currentValue: Double
+    @Binding var targetValue: Int
+    let lowerBorder: Double
+    let upperBorder: Double
+    
+    var body: some View {
+        HStack {
+            ColorValueTextView(value: lowerBorder)
+            TransparencySlider(
+                currentValue: $currentValue,
+                targetValue: $targetValue
+            )
+            .padding()
+            ColorValueTextView(value: upperBorder)
+        }
     }
 }
--- TransparentSliderValidator/TransparencySlider.swift
+++ TransparentSliderValidator/TransparencySlider.swift
@@ -8,24 +8,61 @@
 import SwiftUI
 
 struct TransparencySlider: UIViewRepresentable {
-    @Binding var value: Float
+    @Binding var currentValue: Double
+    @Binding var targetValue: Int
+    @State private var score: Int = 0
     
     func makeUIView(context: Context) -> UISlider {
         let slider = UISlider()
+        slider.thumbTintColor = UIColor(red: 1.0, green: 0, blue: 0, alpha: 1)
         
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.valueChanged),
+            for: .valueChanged
+        )
         return slider
     }
     
     func updateUIView(_ uiView: UISlider, context: Context) {
-        uiView.value = value
+        uiView.value = Float(currentValue)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(currentValue: $currentValue, targetValue: $targetValue, score: $score)
     }
     
     //typealias UIViewType = UISlider
-    
+}
+
+extension TransparencySlider {
+    class Coordinator: NSObject {
+        @Binding var currentValue: Double
+        @Binding var targetValue: Int
+        @Binding var score: Int
+        
+        init(currentValue: Binding<Double>, targetValue: Binding<Int>, score: Binding<Int>) {
+            self._currentValue = currentValue
+            self._targetValue = targetValue
+            self._score = score
+        }
+        
+        @objc func valueChanged(_ sender: UISlider) {
+            currentValue = Double(sender.value)
+            score = computeScore()
+            sender.thumbTintColor = UIColor(red: 1.0, green: 0, blue: 0, alpha: CGFloat(score)/100)
+        }
+        
+        private func computeScore() -> Int {
+            let difference = abs(targetValue - lround(currentValue * 100))
+            return 100 - difference
+        }
+
+    }
 }
 
 struct TransparencySlider_Previews: PreviewProvider {
     static var previews: some View {
-        TransparencySlider(value: .constant(100))
+        TransparencySlider(currentValue: .constant(25), targetValue: .constant(50))
     }
 }
